a4a9d60e1964066a8cfe2807eb60b7cf2fccdef7
Gaurav Sood <gsood07@gmail.com>
2015-09-07 20:20:54 -0400

    better package description within R manual

--- R/abbyy.R
+++ R/abbyy.R
@@ -1,6 +1,9 @@
 #' \pkg{abbyyR} makes OCR easy.
 #'
 #' @name abbyyR
+#' @description Easily OCR images, barcodes, forms, documents with machine readable zones, e.g. passports, right from R.
+#' Get the results in form of text files or detailed XML. The package provides access to Abbyy OCR -- see \url{http://ocrsdk.com/}. 
+#' Details about results of calls to the API can be found at \url{http://ocrsdk.com/documentation/specifications/status-codes/}
 #' @importFrom httr GET POST authenticate config stop_for_status upload_file content
 #' @importFrom XML  xmlToList
 #' @importFrom curl curl_download
