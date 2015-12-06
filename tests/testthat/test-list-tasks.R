9525dfdd4dc7c2ef00ab7122064a5743656deb70
Gaurav Sood <gsood07@gmail.com>
2015-12-06 16:18:19 -0500

    list tasks

--- tests/testthat/test-list-tasks.R
+++ tests/testthat/test-list-tasks.R
@@ -7,13 +7,14 @@ test_that("listTasks happens successfully", {
   setapp(token)
   list_tasks <- listTasks()
   expect_that(listTasks(), is_a("data.frame"))
-  if ( nrow(list_tasks) > 0 ) {
- 	# Task Status
- 	task_status <- getTaskStatus(list_tasks[1,1])
-  expect_that(task_status, is_a("data.frame"))
   
-  # Delete Task
-  del_task <- deleteTask(list_tasks[1,1])
-  expect_that(del_task, is_a("data.frame"))
+  if (nrow(list_tasks) > 0 ) {
+   	# Task Status
+   	task_status <- getTaskStatus(list_tasks[1,1])
+    expect_that(task_status, is_a("data.frame"))
+    
+    # Delete Task
+    del_task <- deleteTask(list_tasks[1,1])
+    expect_that(del_task, is_a("data.frame"))
   }
 })
