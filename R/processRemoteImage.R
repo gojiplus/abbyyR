b5a8e86e3cdccb2de1605bfb08e9ed2f9897e9a0
***** <gsood07@gmail.com>
2016-08-10 14:39:28 -0400

    updated options

--- R/processRemoteImage.R
+++ R/processRemoteImage.R
@@ -9,7 +9,7 @@
 #' @param correctOrientation  Optional; default: true
 #' @param correctSkew Optional; default: true
 #' @param readBarcodes  Optional; default: 
-#' @param exportFormat  Optional; default: txt
+#' @param exportFormat  Optional; default: txt. Must be one of the following: txt, rtf, docx, xlsx, pptx, pdfSearchable, pdfTextAndImages, pdfa, xml, alto
 #' @param pdfPassword  Optional; default: NULL
 #' @param description  Optional; default: ""
 #' @param \dots Additional arguments passed to \code{\link{abbyy_GET}}.
@@ -25,6 +25,8 @@ processRemoteImage <- function(img_url=NULL, language="English", profile="docume
 						correctSkew="true",readBarcodes="false", exportFormat="txt", description=NULL, pdfPassword=NULL, ...){
 	
 	if (is.null(img_url)) stop("Must specify img_url")
+
+	if (!(exportFormat %in% c('txt', 'rtf', 'docx', 'xlsx', 'pptx', 'pdfSearchable', 'pdfTextAndImages', 'pdfa', 'xml', 'alto')))
 
 	querylist = list(source=img_url, language=language, profile=profile,textType=textType, imageSource=imageSource, correctOrientation=correctOrientation, 
 						correctSkew=correctSkew,readBarcodes=readBarcodes,exportFormat=exportFormat, description=description, pdfPassword=pdfPassword)
